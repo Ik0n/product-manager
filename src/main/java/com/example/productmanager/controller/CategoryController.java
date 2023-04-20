0789e41c46d68ded2dbbdee42ca6c4e4e7659eda
Ik0n <Ik0nChannel1@mail.ru>
2023-04-20 12:57:26 +0300

    Small Fix

--- src/main/java/com/example/productmanager/controller/CategoryController.java
+++ src/main/java/com/example/productmanager/controller/CategoryController.java
@@ -1,13 +1,14 @@
 package com.example.productmanager.controller;
 
+import com.example.productmanager.dto.CategoryDTO;
+import com.example.productmanager.dto.ProductDTO;
 import com.example.productmanager.entity.Category;
+import com.example.productmanager.entity.Product;
 import com.example.productmanager.service.CategoryService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -18,9 +19,26 @@ public class CategoryController {
 
     private final CategoryService categoryService;
 
+    @PostMapping
+    public ResponseEntity<Category> create(@RequestBody CategoryDTO dto) {
+        return new ResponseEntity<Category>(categoryService.create(dto), HttpStatus.OK);
+    }
+
     @GetMapping
     public ResponseEntity<List<Category>> readAll() {
         return new ResponseEntity<List<Category>>(categoryService.readAll(), HttpStatus.OK);
     }
+
+    @PutMapping
+    public ResponseEntity<Category> update(@RequestBody Category category) {
+        return new ResponseEntity<Category>(categoryService.update(category), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public HttpStatus delete(@PathVariable Long id) {
+        categoryService.delete(id);
+        return HttpStatus.OK;
+    }
+
 
 }
--- src/main/java/com/example/productmanager/service/CategoryService.java
+++ src/main/java/com/example/productmanager/service/CategoryService.java
@@ -1,6 +1,9 @@
 package com.example.productmanager.service;
 
+import com.example.productmanager.dto.CategoryDTO;
+import com.example.productmanager.dto.ProductDTO;
 import com.example.productmanager.entity.Category;
+import com.example.productmanager.entity.Product;
 import com.example.productmanager.repository.CategoryRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -17,8 +20,22 @@ public class CategoryService {
         return categoryRepository.findAll();
     }
 
+    public Category create(CategoryDTO dto) {
+        return categoryRepository.save(Category.builder()
+                .name(dto.getName())
+                .build());
+    }
+
     public Category readById(Long id) {
         return categoryRepository.findById(id).orElseThrow(() -> new RuntimeException("Category not found " + id));
+    }
+
+    public Category update(Category category) {
+        return categoryRepository.save(category);
+    }
+
+    public void delete(Long id) {
+        categoryRepository.deleteById(id);
     }
 
 }
